e7e9f657ec10c043451f9dc69f4fceff12164193
Kayra Pires Kock da Cunha <kayrakock@gmail.com>
2022-07-20 16:35:20 -0300

    commit inicial

---
+++ etapas.js
@@ -0,0 +1,51 @@
+let etapas = [
+    {
+        titulo: "VEREADOR",
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '26181',
+                nome: 'Lindinha',
+                partido: 'BDC',
+                fotos: [
+                    {url:'3.png', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '18192',
+                nome: 'Pantera Cor-de-Rosa',
+                partido: 'BAC',
+                fotos: [
+                    {url: '4.jpg', legenda: 'Vereador'}
+                ]
+            },
+            ]
+    },
+{
+
+    titulo: 'PREFEITO',
+    numeros: 2,
+    candidatos: [
+        {
+            numero: '22',
+            nome: 'Bob Esponja',
+            partido: 'BDC',
+            vice: 'Mickey',
+            fotos: [
+                {url: '1.png', legenda: 'Prefeito'},
+                {url: '2.png', legenda: 'Vice-Prefeito', small: true}
+            ]
+        },
+        {
+            numero: '84',
+            nome: 'Steet',
+            partido: 'BAC',
+            vice: "Home Simpson",
+            fotos: [
+                {url: '5.png', legenda: 'Prefeito'},
+                {url: '6.jpg', legenda: 'Vice-Prefeito', small:true}
+            ]
+        },
+    ]
+}
+];
